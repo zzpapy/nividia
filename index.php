ab7e3549b6a0df2a13101081057b5cbb94c73228
zzpapy <gregory.pace@hotmail.fr>
2020-02-15 14:11:32 +0100

    main wrapper

--- index.php
+++ index.php
@@ -9,25 +9,107 @@
 </head>
 <body>
     <div class="wrapper">
+        <div class="burger">
+            <i class="fas fa-bars fa-2x"></i>
+        </div>
+        <nav class="nav">
+            <a href="#" class="link">PRODUITS</a>
+            <a href="#" class="link">GAMME RTX</a>
+            <a href="#" class="link"> SUPPORT & PILOTES</a>
+            <a href="#" class="link">CONTACT</a>
+            <i class="close fas fa-times fa-4x"></i>
+        </nav>
         <header class="header">
             <figure class="img_head">            
                 <img src="public/images/nividia.jpg" alt="photo carte graphique">
                 <figcaption>Plus d'informations sur la gamme GeForce RTX</figcaption>
                 <a href="" class="decouvrir">Découvrir</a>
             </figure>
-            <i class="burger fas fa-bars fa-2x"></i>
-                <nav class="nav">
-                    <a href="#" class="link">PRODUITS</a>
-                    <a href="#" class="link">GAMME RTX</a>
-                    <a href="#" class="link"> SUPPORT & PILOTES</a>
-                    <a href="#" class="link">CONTACT</a>
-                    <i class="close fas fa-times fa-4x"></i>
-                </nav>
+            <figure class="img_head_mob"> 
+                <div class="img_mob">
+                    <a href="" >
+                        <img src="public/images/logo.png" alt="logo nividia">
+                    </a>
+                </div>           
+            </figure>
         </header>
         <div class="main">
+            <article class="puissance">
+                <h1 class="titre_puissance">
+                    <div class="img_puissance">
+                        <img src="public/images/logo_vierge.png" alt="logo nividia seul">
+                    </div>
+                    LA PUISSANCE, TOUT SIMPLEMENT
+                </h1>
+                <p class="txt_puissance">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellat, 
+                    asperiores quisquam similique quas tempora deleniti. Rerum a, nihil,
+                    atque voluptate voluptas itaque expedita sequi alias nisi dolorum tenetur quas!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit quam facere animi 
+                    eum ad cum explicabo porro, similique omnis tempore nostrum minima! Quisquam incidunt 
+                    quia minima numquam? Sapiente, sit facilis.
+                </p>
+                <p class="txt_puissance">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellat, 
+                    asperiores quisquam similique quas tempora deleniti. Rerum a, nihil,
+                    atque voluptate voluptas itaque expedita sequi alias nisi dolorum tenetur quas!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit quam facere animi 
+                    eum ad cum explicabo porro, similique omnis tempore nostrum minima! Quisquam incidunt 
+                    quia minima numquam? Sapiente, sit facilis.
+                </p>
+                <p class="txt_puissance">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellat, 
+                    asperiores quisquam similique quas tempora deleniti. Rerum a, nihil,
+                    atque voluptate voluptas itaque expedita sequi alias nisi dolorum tenetur quas!
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit quam facere animi 
+                    eum ad cum explicabo porro, similique omnis tempore nostrum minima! Quisquam incidunt 
+                    quia minima numquam? Sapiente, sit facilis.
+                </p>                
+            </article>
+            <aside class="aside">
+                <article class="article_aside">
+                    <h1 class="titre_aside">A PROPOS
+                        <div class="img_aside">
+                            <img src="public/images/logo.jpg" alt="logo nividia noir">
+                        </div>
+                    </h1>
+                    <p class="txt_aside">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ut quaerat hic nam
+                        explicabo corporis consequuntur dolorem sint officiis libero reprehenderit, incidunt 
+                        aliquam, numquam suscipit labore at. Voluptatibus, corporis dolorum!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ut quaerat hic nam
+                        explicabo corporis consequuntur dolorem sint officiis libero reprehenderit, incidunt 
+                        aliquam, numquam suscipit labore at. Voluptatibus, corporis dolorum!
+                    </p>
+                    <p class="txt_aside">aliquam, numquam suscipit labore at. Voluptatibus, corporis dolorum!</p>
+                    <p class="txt_aside">aliquam, numquam suscipit</p>
+                    <div class="social">
+                        <i class="fab fa-facebook-f fa-2x"></i>
+                        <i class="fab fa-twitter fa-2x"></i>
+                        <i class="fab fa-instagram fa-2x"></i>
+                        <i class="fab fa-linkedin-in fa-2x"></i>
+                        <i class="fab fa-snapchat-ghost fa-2x"></i>
+                    </div>
+                </article>
+            </aside>
         </div>
     </div>
     <footer class="footer">
+        <div class="footer_section">
+            <h2 class="titre_container"></h2>
+            <div class="container_footer">
+                <input type="text" class="input" placeholder="Nom">
+                <input type="text" class="input" placeholder="Email">
+            </div>
+        </div>
+        <div class="footer_section">
+            <div class="container_footer"></div>
+            <h2 class="titre_container"></h2>
+        </div>
+        <div class="footer_section">
+            <div class="container_footer"></div>
+            <h2 class="titre_container"></h2>
+        </div>
     </footer>
     <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,18 @@
 $(".burger").click(function(){
     $(".nav").css("display","flex");
+    $(".nav").css("position","sticky");
+    $(".nav").css("top","0");
+    $(".nav").fadeTo( "slow" , 0.5);
     $(".nav").css("flex-direction","column");
+    $(".burger").css("display","none");
 })
 $(".close").click(function(){
     console.log("'toto");
     $(".nav").css("display","none");
+    $(".burger").css("display","block");
+})
+$(".nav a").click(function(){
+    console.log("'toto");
+    $(".nav").css("display","none");
+    $(".burger").css("display","block");
 })
